6ba5237d728b239d68a0bc20f8e508825767a120
andregsistemas <andregsistemas@outlook.com>
2024-08-21 11:36:49 -0300

    04.Adicionando imagens com o FileReader

--- src/app/paginas/perfil-contato/perfil-contato.component.ts
+++ src/app/paginas/perfil-contato/perfil-contato.component.ts
@@ -1,25 +1,49 @@
-import { Component } from '@angular/core';
+import { ContatoService } from './../../services/contato.service';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContainerComponent } from '../../componentes/container/container.component';
 import { Contato } from '../../componentes/contato/contato';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-perfil-contato',
   standalone: true,
   imports: [
     CommonModule,
-    ContainerComponent
+    ContainerComponent,
+    RouterLink
   ],
   templateUrl: './perfil-contato.component.html',
   styleUrl: './perfil-contato.component.css'
 })
-export class PerfilContatoComponent {
+export class PerfilContatoComponent implements OnInit {
   contato: Contato = {
     id: 0,
-    nome: 'dev',
-    telefone: '123456789',
-    email: 'teste@teste.com',
-    aniversario: '27/12/1985',
-    redes: 'teste.com'
+    nome: '',
+    telefone: '',
+    email: '',
+    aniversario: '',
+    redes: ''
   }
+
+  constructor(private activatedRoute: ActivatedRoute, private contatoService: ContatoService, private router: Router) { }
+
+  ngOnInit() {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    if (id) {
+      this.contatoService.buscarPorId(parseInt(id)).subscribe((contato) => {
+        this.contato = contato;
+      });
+    }
+
+  }
+  excluir() {
+    if (this.contato.id) {
+      this.contatoService.excluirContato(this.contato.id).subscribe(() => {
+        this.router.navigateByUrl('/lista-contatos')
+      })
+    }
+
+  }
+
 }
